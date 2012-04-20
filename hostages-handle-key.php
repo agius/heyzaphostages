f902588e0a20e08bd0b588399d58a8099d84422b
Andrew Evans <andrew@atevans.com>
2012-04-20 13:52:50 -0700

    mp3s, standard

--- hostages-handle-key.php
+++ hostages-handle-key.php
@@ -12,31 +12,9 @@
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-<?php if ($_REQUEST['Digits'] == '98314335') { ?>
-  <Say>Your first clue is: We see Heyzap awesomeness every week. But what's BEHIND the awesomeness?</Say>
-<?php } elseif ($_REQUEST['Digits'] == '61034608') { ?>
-  <Say>good guess, but the real secret is what's on the inside. Something... alcoholic.</Say>
-<?php } elseif ($_REQUEST['Digits'] == '97372993') { ?>
-  <Say>now you're getting it. your next clue is behind the least-used computer in the office</Say>
-<?php } elseif ($_REQUEST['Digits'] == '56303035') { ?>
-  <Say>the next clue requires a green thumb, and a power cord</Say>
-<?php } elseif ($_REQUEST['Digits'] == '49076981') { ?>
-  <Say>you'd better hurry, or i will kill one of the hostages. the paperless office is a universal dream. your next clue is a nightmare</Say>
-<?php } elseif ($_REQUEST['Digits'] == '29175769') { ?>
-  <Say>you're doing well. the clues will get harder from here. you may need to use some collective intelligence to find the next one</Say>
-<?php } elseif ($_REQUEST['Digits'] == '73433093') { ?>
-  <Say>the next clue is a hairy one - red and hairy</Say>
-<?php } elseif ($_REQUEST['Digits'] == '76145273') { ?>
-  <Say>if you're not an Englishman, you must be a Bitter American</Say>
-<?php } elseif ($_REQUEST['Digits'] == '19626037') { ?>
-  <Say>from here you can see dinosaurs, plumbers, and how you're a pawn in my little game</Say>
-<?php } elseif ($_REQUEST['Digits'] == '28836890') { ?>
-  <Say>almost there.
-  your last clue is in cryostasis</Say>
-<?php } elseif ($_REQUEST['Digits'] == '13143233') { ?>
-  <Say>congratulations! the countdown timer has five minutes left
-  the best view isn't in the office, it's up the corporate ladder</Say>
-<?php } else { ?>
-  <Say>welcome to my little game. if you win, you will get a reward. if you do not, the hostages will die. your first code is 9 8 3 1 4 3 3 5</Say>
-<?php } ?>
+  <?php if(file_exists($_SERVER{'DOCUMENT_ROOT'} . "/my_images/". $_REQUEST['Digits'] . ".mp3")) { ?>
+    <Play><?php echo $_SERVER{'DOCUMENT_ROOT'} . "/my_images/". $_REQUEST['Digits'] . ".mp3" ?></Play>
+  <?php } else { ?>
+    <Say>That was an invalid code.</Say>
+  <?php } ?>
 </Response>
