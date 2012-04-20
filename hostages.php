7d43fc503d734f863bedc5ca37b9f534034932e1
Andrew Evans <andrew@atevans.com>
2012-04-20 12:18:45 -0700

    hostages.php

---
+++ hostages.php
@@ -0,0 +1,9 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Say>
+      If you do not complete the challenges by lunch time, the hostages will die. If you can find them, you will receive a reward. Please enter your code.
+    </Say>
+</Response>
